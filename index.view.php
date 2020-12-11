b2969a1ab092c33e0eff02f2807b8c5d7e595331
Guardian-75 <htetphyolin46@gmail.com>
2020-12-12 04:20:11 +0630

    test

---
+++ index.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intial=scale=1.0">
+    <title></title>
+</head>
+
+<style> 
+    h1{
+        align: center;
+        background-color: grey;
+        padding: 2em;
+    }
+</style>
+
+<body>
+
+
+</body>
+</html>
